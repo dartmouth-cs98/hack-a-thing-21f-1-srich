365de9b041f6e6c514e44edb2b3464443963a296
sasharich <58890794+sasharich@users.noreply.github.com>
2021-09-22 15:14:44 -0400

    Added an array and a for loop to populate the array.

--- hack.go
+++ hack.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	//"hash"
+	//"list"
+	"math"
 	/*
 	"math"
-	"hash"
 	"runtime"
 	"time"
 	"sort"
@@ -12,5 +14,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, CS98! Its so nice to meet you! I am Sasha's super cool program made while learning Golang!")
+	fmt.Println("Hello, CS98! Its so nice to meet you! I am Sasha's super cool program made while learning Golang! This message was printed to you using the fmt directory which allows you to take inputs and produce outputs!")
+	
+	var myCoolArray [25]int 
+	for i := 0; i < 25; i++ {
+		myCoolArray[i] = i
+	}
+
+/*
+	//create new hashtable
+	const arrayLength = 50
+	type hashtable struct {
+
+		data[arrayLength]*linkedlist.LinkedList
+	}
+*/
+	
 }
